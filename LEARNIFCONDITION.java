dda522b9fdc4e2211771363df1a11ecafee4fe90
jp7020448 <jp7020448@gmail.com>
2025-12-04 16:01:50 +0530

    Add files via upload

---
+++ LEARNIFCONDITION.java
@@ -0,0 +1,18 @@
+package basicjava;
+
+public class LEARNIFCONDITION {
+	public static void main(String[]args) { 
+	int number=10;
+	if(number>0) {
+		System.out.println("The number is positive");
+	
+	}
+	else if(number<0) {
+		System.out.println("The number is negative");
+	}
+	else {
+		System.out.println("The Number is zero");
+		
+	}
+}
+}
