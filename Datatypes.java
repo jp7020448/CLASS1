6699d3c775b97806d61ecda45048f158adddba4a
jp7020448 <jp7020448@gmail.com>
2025-12-04 15:26:11 +0530

    Add files via upload
    DATATYPES

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+package basicjava;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int
+		int age0fPerson=30;
+		char logo='t';
+		String name="JAYAPRAKASH";
+		boolean value=true;
+		System.out.println(name);
+		
+	}
+
+}
