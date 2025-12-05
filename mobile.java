530e25acf5fdfe8311442b6b67631b80f0623e2b
jp7020448 <jp7020448@gmail.com>
2025-12-05 10:28:50 +0530

    mobile

---
+++ mobile.java
@@ -0,0 +1,16 @@
+package basicjava;
+
+public class mobile {
+	
+	public void takeselfie() {
+		System.out.println("take selfie");
+		
+	}
+
+	public static void main(String[] args) {
+		mobile obj = new mobile();
+		obj.takeselfie();
+
+	}
+
+}
